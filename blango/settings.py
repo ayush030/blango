ab219a64b99190a5e6868bd227b0c17b40c51baf
Ayush Tripathi <ayushtripathi@bel.co.in>
2022-04-26 19:43:41 +0000

    Finish html frameworks

--- blango/settings.py
+++ blango/settings.py
@@ -15,8 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
+# print(BASE_DIR)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -62,10 +61,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'blango.urls'
 
+# addition of template base.html
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
