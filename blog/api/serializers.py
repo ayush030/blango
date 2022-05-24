e6b3aa45a1492ddac8e3057004899ecfdf047cd6
Ayush Tripathi <ayushtripathi@bel.co.in>
2022-05-24 02:17:43 +0000

    TagField in postserializer

--- blog/api/serializers.py
+++ blog/api/serializers.py
@@ -2,8 +2,17 @@ from rest_framework import serializers
 from blog.models import Post, Tag, Comment
 from blango_auth.models import User
 
+
+class TagField(serializers.SlugRelatedField):
+  def to_internal_value(self, data):
+    try:
+      return self.get_queryset().get_or_create(value=data.lower())[0]
+    except(TypeError, ValueError):
+      self.fail(f"Tag value {data} is invalid")
+
+
 class PostSerializer(serializers.ModelSerializer):
-  tags = serializers.SlugRelatedField(
+  tags =TagField(
         slug_field="value", many=True, queryset=Tag.objects.all()
     )
   author = serializers.HyperlinkedRelatedField(
@@ -21,13 +30,6 @@ class UserSerializer(serializers.ModelSerializer):
     model = User
     fields = ["first_name", "last_name", "email"]
 
-
-class TagField(serializers.SlugRelatedField):
-  def to_internal_value(self, data):
-    try:
-      return self.get_queryset().get_or_create(value=data.lower())[0]
-    except(TypeError, ValueError):
-      self.fail(f"Tag value {data} is invalid")
 
 class CommentSerializer(serializers.ModelSerializer):
   id = serializers.IntegerField(required=False) #if its a new comment this will be empty
