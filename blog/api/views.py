bae88e331cf1f1a296f754551aa774e644ceb143
Ayush Tripathi <ayushtripathi@bel.co.in>
2022-06-06 22:39:57 +0000

    finished caching

--- blog/api/views.py
+++ blog/api/views.py
@@ -22,6 +22,14 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 
 
+#caching to viewsets or generic class based views( use of method_decorator)
+#  The method_decorator decorator transforms a function decorator into a method decorator so that it can be used on an instance method.
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_headers, vary_on_cookie
+
+from rest_framework.exceptions import PermissionDenied 
+
 #generic implementation using APIView 
 
 # class PostList(generics.ListCreateAPIView):
@@ -54,12 +62,33 @@ class PostViewSet(viewsets.ModelViewSet):
     if self.action in ("list", "create"):
       return PostSerializer
     return PostDetailSerializer
+  
+  @method_decorator(cache_page(300))
+  @method_decorator(vary_on_headers("Authorization"))
+  @method_decorator(vary_on_cookie)
+  #method_decorator(vary_on_headers("Authorization", "Cookie"))  #alternatively
+  @action(methods=["get"], detail=False, name="Posts by the logged in user")
+  def mine(self, request):
+    if request.user.is_anonymous:
+      raise PermissionDenied("You must be logged in to see which Posts are yours")
+    posts = self.get_queryset().filter(author=request.user)
+    serializer = PostSerializer(posts, many=True, context={"request":request})
+    return Response(serializer.data)
+  
+  # adding caching to methods implemented/available with viewset by passthrough same methods using super class
+  @method_decorator(cache_page(300))
+  def list(self, *args, **kwargs):
+    return super(PostViewSet, self).list(*args, **kwargs)
 
 
 class UserDetail(generics.RetrieveAPIView):
     lookup_field = "email"
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    @method_decorator(cache_page(300))
+    def get(self, *args, **kwargs):
+      return super(UserDetail, self).get(*args, **kwargs)
 
 
 #Tag viewset
@@ -83,3 +112,12 @@ class TagViewSet(viewsets.ModelViewSet):
     post_serializer = PostSerializer(tag.posts, many=True, context={"request":request})
     #--------------------how tag.posts is retrieved-------------------------------
     return Response(post_serializer.data)
+
+
+  @method_decorator(cache_page(300))
+  def list(self, *args, **kwargs):
+    return super(TagViewSet, self).list(*args, **kwargs)
+
+  @method_decorator(cache_page(300))
+  def retrieve(self, *args, **kwargs):
+    return super(TagViewSet, self).retrieve(*args, **kwargs)
